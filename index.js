a96793112629a8705a232e6c075118ca32c05c01
Dominic8999 <dominic3331@fedora.email>
2023-08-04 17:58:31 +0200

    today - august 4th

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// Increase
+function updateNumber(elementId, operation) {
+  var numEl = document.querySelector(elementId);
+  var num = parseInt(numEl.textContent);
+  
+  if (operation === "inc") {
+    num += 1;
+  } else if (operation === "dec") {
+    num -= 1;
+  } else if (operation === "reset") {
+    num = 0;
+  }
+
+  numEl.textContent = num;
+  console.log("The number was updated");
+}
+
 function inc1() {
-  var num1El = document.querySelector("#p-1");
-  var num1 = parseInt(num1El.textContent);
-  num1 += 1;
-  num1El.textContent = num1;
-  console.log("The number was increased by +1");
+  updateNumber("#p-1", "inc");
 }
 
 function inc2() {
-  var num2El = document.querySelector("#p-2");
-  var num2 = parseInt(num2El.textContent);
-  num2 += 1;
-  num2El.textContent = num2;
-  console.log("The number was increased by +1");
+  updateNumber("#p-2", "inc");
 }
 
-// Decrease
 function dec1() {
-  var num1decEl = document.querySelector("#p-1");
-  var num1dec = parseInt(num1decEl.textContent);
-  num1dec -= 1;
-  num1decEl.textContent = num1dec;
-  console.log("The number was decreased by -1");
-}
-function dec2() {
-  var num2decEl = document.querySelector("#p-2");
-  var num2dec = parseInt(num2decEl.textContent);
-  num2dec -= 1;
-  num2decEl.textContent = num2dec;
-  console.log("The number was decreased by -1");
+  updateNumber("#p-1", "dec");
 }
 
-// reset 
-function reset1() {
-  var num1resetEl = document.querySelector("#p-1");
-  var num1reset = 0;
-  num1resetEl.textContent = num1reset;
-  console.log("The first number was reset");
+function dec2() {
+  updateNumber("#p-2", "dec");
 }
+
+function reset1() {
+  updateNumber("#p-1", "reset");
+}
+function reset2() {
+  updateNumber("#p-2", "reset");
+}
+
+// Calculator options:
