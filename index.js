52d830ae3e6ea714724e51f5611bf3b25077ca11
Dominic8999 <dominic3331@fedora.email>
2023-08-04 13:34:22 +0200

    yes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-
+// Increase
 function inc1() {
   var num1El = document.querySelector("#p-1");
   var num1 = parseInt(num1El.textContent);
@@ -9,9 +8,25 @@ function inc1() {
 }
 
 function inc2() {
-  let num2El = document.getElementById("p2");
+  var num2El = document.querySelector("#p-2");
   var num2 = parseInt(num2El.textContent);
   num2 += 1;
   num2El.textContent = num2;
   console.log("The number was increased");
+}
+
+// Decrease
+function dec1() {
+  var num1decEl = document.querySelector("#p-1");
+  var num1dec = parseInt(num1decEl.textContent);
+  num1dec -= 1;
+  num1decEl.textContent = num1dec;
+  console.log("The number was decreased by 1");
+}
+function dec2() {
+  var num2decEl = document.querySelector("#p-2");
+  var num2dec = parseInt(num2decEl.textContent);
+  num2dec -= 1;
+  num2decEl.textContent = num2dec;
+  console.log("The number was decreased by 1");
 }
