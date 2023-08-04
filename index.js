07a8f964b978bbb12637223c82983a7f5c3e5948
Dominic8999 <dominic3331@fedora.email>
2023-08-04 12:43:19 +0200

    yes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let num2El = document.getElementById("p2");
-let num1El = document.getElementById("p1");
 
-let num1 = 0;
-let num2 = 0;
 
 function inc1() {
+  var num1El = document.querySelector("#p-1");
+  var num1 = parseInt(num1El.textContent);
   num1 += 1;
   num1El.textContent = num1;
   console.log("The number was increased");
 }
 
 function inc2() {
+  let num2El = document.getElementById("p2");
+  var num2 = parseInt(num2El.textContent);
   num2 += 1;
   num2El.textContent = num2;
   console.log("The number was increased");
