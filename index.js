469f340dfbc608f4da8dfe3bf5928fd68c35e875
Dominic8999 <dominic3331@fedora.email>
2023-08-04 14:00:36 +0200

    yes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function inc1() {
   var num1 = parseInt(num1El.textContent);
   num1 += 1;
   num1El.textContent = num1;
-  console.log("The number was increased");
+  console.log("The number was increased by +1");
 }
 
 function inc2() {
@@ -12,7 +12,7 @@ function inc2() {
   var num2 = parseInt(num2El.textContent);
   num2 += 1;
   num2El.textContent = num2;
-  console.log("The number was increased");
+  console.log("The number was increased by +1");
 }
 
 // Decrease
@@ -21,12 +21,20 @@ function dec1() {
   var num1dec = parseInt(num1decEl.textContent);
   num1dec -= 1;
   num1decEl.textContent = num1dec;
-  console.log("The number was decreased by 1");
+  console.log("The number was decreased by -1");
 }
 function dec2() {
   var num2decEl = document.querySelector("#p-2");
   var num2dec = parseInt(num2decEl.textContent);
   num2dec -= 1;
   num2decEl.textContent = num2dec;
-  console.log("The number was decreased by 1");
+  console.log("The number was decreased by -1");
+}
+
+// reset 
+function reset1() {
+  var num1resetEl = document.querySelector("#p-1");
+  var num1reset = 0;
+  num1resetEl.textContent = num1reset;
+  console.log("The first number was reset");
 }
