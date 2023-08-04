9ac894be8b0d85488471e4cb93da2524e641bd39
Dominic8999 <dominic3331@fedora.email>
2023-08-04 23:14:48 +0200

    finallly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function updateNumber(elementId, operation) {
   var numEl = document.querySelector(elementId);
   var num = parseInt(numEl.textContent);
-  
+
   if (operation === "inc") {
     num += 1;
   } else if (operation === "dec") {
@@ -33,8 +33,63 @@ function dec2() {
 function reset1() {
   updateNumber("#p-1", "reset");
 }
+
 function reset2() {
   updateNumber("#p-2", "reset");
 }
 
-// Calculator options:
+function add() {
+  var numEl1 = document.querySelector("#p-1");
+  var numEl2 = document.querySelector("#p-2");
+  var resultEl = document.querySelector("#p-3");
+
+  var num1 = parseInt(numEl1.textContent);
+  var num2 = parseInt(numEl2.textContent);
+
+  var result = num1 + num2;
+
+  resultEl.textContent = result;  // Update the result paragraph
+  console.log("Numbers were added");
+}
+
+function substract() {
+  var numEl1 = document.querySelector("#p-1");
+  var numEl2 = document.querySelector("#p-2");
+  var resultEl = document.querySelector("#p-3");
+
+  var num1 = parseInt(numEl1.textContent);
+  var num2 = parseInt(numEl2.textContent);
+
+  var result = num1 - num2;
+
+  resultEl.textContent = result;  // Update the result paragraph
+  console.log("Numbers were added");
+}
+
+function divide() {
+  var numEl1 = document.querySelector("#p-1");
+  var numEl2 = document.querySelector("#p-2");
+  var resultEl = document.querySelector("#p-3");
+
+  var num1 = parseInt(numEl1.textContent);
+  var num2 = parseInt(numEl2.textContent);
+
+  var result = num1 / num2;
+
+  resultEl.textContent = result;  // Update the result paragraph
+  console.log("Numbers were added");
+}
+
+function multiply() {
+  var numEl1 = document.querySelector("#p-1");
+  var numEl2 = document.querySelector("#p-2");
+  var resultEl = document.querySelector("#p-3");
+
+  var num1 = parseInt(numEl1.textContent);
+  var num2 = parseInt(numEl2.textContent);
+
+  var result = num1 * num2;
+
+  resultEl.textContent = result;  // Update the result paragraph
+  console.log("Numbers were added");
+}
